0e5013f38cddf49170a8573219ce68fca483a9aa
Nedas <nedas.zilinskas@gmail.com>
2018-07-04 18:57:13 +0300

    [ADD] [11.0] [currency_rate_update_by_nbb]

---
+++ currency_rate_update_by_nbb/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2018 Ventor, Xpansa Group <https://ventor.tech/>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Currency Rate Update National Bank Of Belarus",
+    "summary": "Download currency exchange rates from National Bank Of Belarus",
+    "version": "11.0.1.0.0",
+    "category": "Accounting & Finance",
+    "website": "https://github.com/xpansa/l10n-belarus",
+    "author": "Ventor, Xpansa Group, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "currency_rate_update",
+    ],
+}

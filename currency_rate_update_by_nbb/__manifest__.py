38b307b328c51882925990f73de464be5f4ff8da
Nedas Zilinskas <nedas.zilinskas@gmail.com>
2018-07-04 20:43:21 +0300

    [LINT] [11.0] [currency_rate_update_by_nbb]

--- currency_rate_update_by_nbb/__manifest__.py
+++ currency_rate_update_by_nbb/__manifest__.py
@@ -2,7 +2,8 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Currency Rate Update National Bank Of Belarus",
-    "summary": "Download currency exchange rates from National Bank Of Belarus",
+    "summary":
+    "Download currency exchange rates from National Bank Of Belarus",
     "version": "11.0.1.0.0",
     "category": "Accounting & Finance",
     "website": "https://github.com/xpansa/l10n-belarus",
